b69673de8e99b061fe5ca629c5a588f235d6db9b
Steven Koolhaas <snack93@lavabit.com>
2014-01-08 12:24:49 +0100

    update

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -12,8 +12,13 @@ class StatsController < ApplicationController
 		# Set it up
 		entry_stats = Hash.new # brand and type sorted statuses
 		entry_stats_global = Array.new # global statuses
+		entry_stats_status = Hash.new # test/repair/scrap statuses
+		entry_stats_status_global = Array.new # test/repair/scrap global statuses
 		(0..6).each do |i| #0..'amount of statuses'
 			entry_stats_global[i] = 0
+		end
+		(0..2).each do |i| #0..'amount of statuses'
+			entry_stats_status_global[i] = 0
 		end
 		entries = Entry.where(company: session[:company])
 
@@ -22,30 +27,64 @@ class StatsController < ApplicationController
 			# unknown brand
 			unless entry_stats.has_key? entry.brand
 				entry_stats[entry.brand] = Hash.new
+				entry_stats_status[entry.brand] = Hash.new
 			end
 			# unknown type
 			unless entry_stats[entry.brand].has_key? entry.typenum
 				entry_stats[entry.brand][entry.typenum] = Array.new
+				entry_stats_status[entry.brand][entry.typenum] = Array.new
 				(0..6).each do |i| #0..'amount of statuses'
 					entry_stats[entry.brand][entry.typenum][i] = 0
+				end
+				(0..2).each do |i| #0..'amount of statuses'
+					entry_stats_status[entry.brand][entry.typenum][i] = 0
 				end
 			end
 
 			entry_status = Stats.new.process_status(entry)
 			entry_stats[entry.brand][entry.typenum][entry_status] = entry_stats[entry.brand][entry.typenum][entry_status] + 1
 			entry_stats_global[entry_status] = entry_stats_global[entry_status] + 1
+
+			if entry.scrap == 1
+				entry_stats_status[entry.brand][entry.typenum][2] = entry_stats_status[entry.brand][entry.typenum][2] + 1
+				entry_stats_status_global[2] = entry_stats_status_global[2] + 1
+			elsif entry.repair == 1
+				entry_stats_status[entry.brand][entry.typenum][1] = entry_stats_status[entry.brand][entry.typenum][1] + 1
+				entry_stats_status_global[1] = entry_stats_status_global[1] + 1
+			elsif entry.test == 1
+				entry_stats_status[entry.brand][entry.typenum][0] = entry_stats_status[entry.brand][entry.typenum][0] + 1
+				entry_stats_status_global[0] = entry_stats_status_global[0] + 1
+			end
 		end
 
 		# Generate chart data
-		@charts = Array.new
-		@charts.append Stats.new.generate_chart(session[:company], entries.length.to_s, entry_stats_global) # global chart first
+		@charts = Hash.new
+		@charts[:global] = Array.new
+		@charts[:global].append Stats.new.generate_chart(session[:company], entries.length.to_s, entry_stats_global) # global chart first
+		@charts[:global].append Stats.new.generate_chart_status(session[:company], entries.length.to_s, entry_stats_status_global) # global chart first
 		entry_stats.each_pair do |brand, brand_data|
 			brand_data.each_pair do |type, type_data|
 				type_total = 0
 				type_data.each do |i|
 					type_total += i
 				end
-			    @charts.append Stats.new.generate_chart(brand + ' ' + type, type_total.to_s, type_data) # each brand and type sorted chart after
+				unless @charts.has_key? (brand + type)
+					@charts[brand + type] = Array.new
+				end
+			    @charts[brand + type].append Stats.new.generate_chart(brand + ' ' + type, type_total.to_s, type_data) # each brand and type sorted chart after
+			end
+		end
+		entry_stats_status.each_pair do |brand, brand_data|
+			brand_data.each_pair do |type, type_data|
+				unless ((type_data[0] + type_data[1] + type_data[2]) == 0)
+					type_total = 0
+					type_data.each do |i|
+						type_total += i
+					end
+			    	@charts[brand + type].append Stats.new.generate_chart_status(brand + ' ' + type, type_total.to_s, type_data) # each brand and type sorted chart after
+				else
+					@charts[brand + type].append LazyHighCharts::HighChart.new('pie')
+				end
 			end
 		end
 	end
--- app/models/stats.rb
+++ app/models/stats.rb
@@ -31,21 +31,74 @@ class Stats < ActiveRecord::Base
 
   # generates a chart based on title and data
   def generate_chart title, subtitle, data
-    chart = LazyHighCharts::HighChart.new('column') do |f|
-      f.chart({ type: 'column', marginRight: 130, marginBottom: 25 })
-      f.title({ text: 'Status for ' + title, x: -20 })
-      f.yAxis({ title: { text: 'Amount' }, plotLines: [{ value: 0, width: 1, color: '#808080' }] })
-      f.xAxis({ categories: ['Status - Total: ' + subtitle] })
-      f.legend({ layout: 'vertical', align: 'right', verticalAlign: 'top', x: -10, y: 100, borderWidth: 0 })
-      
-      f.series({ name: 'New', data: [data[0].to_f] })
-      f.series({ name: 'Tested', data: [data[1].to_f] })
-      f.series({ name: 'Repaired', data: [data[2].to_f] })
-      f.series({ name: 'Scrap', data: [data[3].to_f] })
-      f.series({ name: 'Waiting', data: [data[4].to_f] })
-      f.series({ name: 'Ready', data: [data[5].to_f] })
-      f.series({ name: 'Sent', data: [data[6].to_f] })
+    chart = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie"} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Amount',
+               :data=> [
+                  [ 'New', data[0].to_f ],
+                  [ 'Tested', data[1].to_f ],
+                  [ 'Repaired', data[2].to_f ],
+                  [ 'Scrap', data[3].to_f ],
+                  [ 'Waiting', data[4].to_f ],
+                  [ 'Ready', data[5].to_f ],
+                  {
+                     :name=> 'Sent',    
+                     :y=> data[6].to_f,
+                     :sliced=> true,
+                     :selected=> true
+                  }
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = 'Status for ' + title
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
     end
   end
 
+  def generate_chart_status title, subtitle, data
+    chart = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({:defaultSeriesType=>"pie"} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Amount',
+               :data=> [
+                  [ 'Tested', data[0].to_f ],
+                  [ 'Repaired', data[1].to_f ],
+                  {
+                     :name=> 'Scrap',    
+                     :y=> data[2].to_f,
+                     :sliced=> true,
+                     :selected=> true
+                  }
+               ]
+      }
+      f.series(series)
+      f.options[:title][:text] = 'Status for ' + title
+      f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+      f.plot_options(:pie=>{
+        :allowPointSelect=>true, 
+        :cursor=>"pointer" , 
+        :dataLabels=>{
+          :enabled=>true,
+          :color=>"black",
+          :style=>{
+            :font=>"13px Trebuchet MS, Verdana, sans-serif"
+          }
+        }
+      })
+    end
+  end
 end
