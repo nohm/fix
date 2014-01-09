d55c2f5fe943fbba2708714f8618deed2063e1d3
Steven Koolhaas <snack93@lavabit.com>
2014-01-09 11:34:55 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,7 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+user.remove_role :user
+puts 'DEFAULT CLASS'
+classification = Classifications.find_or_create_by_name :name => 'N/A'.dup
+puts 'class: ' << classification.name
