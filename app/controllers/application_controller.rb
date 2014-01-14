e6fbb7e44c8767fa5ed3f3a1420b089e7f7669e9
Steven Koolhaas <s.snack93@gmail.com>
2014-01-14 01:05:24 +0100

    moved subtitle back into naw, made status admin only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
-  before_filter :role_list, :request_timer, :build_subtitle
+  before_filter :role_list, :request_timer
   def role_list
   	unless current_user.nil?
 	    if current_user.has_role? :technician or current_user.has_role? :manager or current_user.has_role? :admin
@@ -21,16 +21,4 @@ class ApplicationController < ActionController::Base
   def request_timer
     @time = Time.now.to_f
   end
-
-  def build_subtitle
-    @subtitle = ''
-    if user_signed_in?
-      @subtitle = current_user.name
-    end
-    unless session[:company].nil?
-      @subtitle = @subtitle + ' @ ' + session[:company].titleize
-    end
-    @subtitle
-  end
-
 end
