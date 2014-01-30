4d22f6977be34d5a89409d2febdd5dcbf0ccaf0a
Steven Koolhaas <snack93@lavabit.com>
2014-01-30 08:59:12 +0100

    Updating server-side routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,33 @@
 Badger::Application.routes.draw do
-  get "broadcast/index"
-  get "broadcast/new"
   root :to => "home#index"
 
   devise_for :users, :controllers => {:registrations => "users/registrations"}
-
   resources :users
-  resources :companies do
+
+  resources :companies, :only => [:index, :new, :create, :destroy] do
     resources :stats, :only => :index
-    resources :invoices
+    resources :invoices, :only => [:index, :new, :create, :destroy]
     resources :entries do
-  	  resources :attachments
+  	  resources :attachments, :only => [:create, :destroy]
     end
   end
-  resources :appliances
-  resources :classifications
-  resources :broadcasts
-
-  match 'broadcast/disable/:id' => 'broadcasts#disable', :as => :disable_broadcast, :via => :get
 
   # Special routes for batch updating
   match 'companies/:company_id/batch' => 'home#batch', :as => :batch, :via => :get
   match 'companies/:company_id/batch' => 'home#batch_update', :as => :batch_update, :via => :post
 
-  # Special routes for error reporting
-  match 'report' => 'home#report', :as => :report, :via => :get
-  match 'send_report' => 'home#send_report', :as => :send_report, :via => :post
-
   # Special routes for entries
   match 'companies/:company_id/entries/:id/sticker' => 'entries#sticker', :as => :sticker, :via => :get
   match 'companies/:company_id/entries/:id/ticket' => 'entries#ticket', :as => :ticket, :via => :get
   match 'companies/:company_id/entries/:id/zip' => 'entries#zip', :as => :zip, :via => :get
+
+  resources :appliances, :only => [:index, :new, :create, :destroy]
+  resources :classifications, :only => [:index, :new, :create]
+
+  resources :broadcasts, :only => [:index, :new, :create, :destroy]
+  match 'broadcast/disable/:id' => 'broadcasts#disable', :as => :disable_broadcast, :via => :get
+
+  # Special routes for error reporting
+  match 'report' => 'home#report', :as => :report, :via => :get
+  match 'send_report' => 'home#send_report', :as => :send_report, :via => :post
 end
