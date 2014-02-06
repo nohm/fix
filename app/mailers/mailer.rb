7506f7ae25144fe96936e9db50fbeaede2ed4f36
Steven Koolhaas <snack93@lavabit.com>
2014-02-06 19:28:14 +0100

    translated missing stuff in mailer, added open-sans font

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -5,24 +5,24 @@ class Mailer < ActionMailer::Base
     @name = name
     @email = email
     @url = "#{ENV['BASE_URL']}/badger"
-    mail(to: email, subject: 'Welcome to Badger, ' + name)
+    mail(to: email, subject: t('mailer.base.welcome') + name)
   end
 
   def send_new_user_message(email)
     @email = email
     @url = "#{ENV['BASE_URL']}/badger/users"
-    mail(to: ENV["GMAIL_USERNAME"], subject: 'New sign up for Badger')
+    mail(to: ENV["GMAIL_USERNAME"], subject: t('mailer.base.new_user'))
   end
 
   def send_error_report(email, message)
     @email = email
     @message = message
-    mail(to: ENV["GMAIL_USERNAME"], subject: 'Badger: Error report')
+    mail(to: ENV["GMAIL_USERNAME"], subject: t('mailer.base.error_report'))
   end
 
   def send_role_update(role)
     @role = role
     @url = "#{ENV['BASE_URL']}/badger"
-    mail(to: email, subject: 'Badger: Role updated')
+    mail(to: email, subject: t('mailer.base.role_update'))
   end
 end
