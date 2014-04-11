36632515aa76bfb5ddcf01661d1a256f3b5cec1f
Steven Koolhaas <snack93@lavabit.com>
2014-04-11 18:41:16 +0200

    removed line that prevented appliances from saving

--- app/models/appliance.rb
+++ app/models/appliance.rb
@@ -3,7 +3,7 @@ class Appliance < ActiveRecord::Base
 
   validates :name, presence: true
   validates :abb, presence: true
-  validates :preview, presence: true
+  #validates :preview, presence: true
 
   validates_uniqueness_of :name
   validates_uniqueness_of :abb
