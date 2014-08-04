b2987f2c3c6c9c366dc3a0572cfc1e4cf36ca7c8
Steven Koolhaas <snack93@lavabit.com>
2014-08-04 15:05:05 +0200

    session store cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Fix::Application.config.session_store :cookie_store, key: '_badger_session'
+Fix::Application.config.session_store :cookie_store, key: '_fix_session'
